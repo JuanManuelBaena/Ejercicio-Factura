8b09153b3c86667169cb59f4293fbfb6411715e9
Juan Manuel Baena <jbaeurb3009@g.educaand.es>
2025-09-25 12:58:44 +0200

    Fix: Alineacion a la derecha y linea de unitario

--- src/App.java
+++ src/App.java
@@ -8,23 +8,24 @@ public class App {
         System.out.printf("%-30s%-30s%-30s%30s%n", "Facturar A", "Enviar A", "Nº DE FACTURA", "ES-001");
         System.out.printf("%-30s%-30s%-30s%30s%n", "Leda Villareal", "Leda Villareal", "FECHA", "29.01.2019");
         System.out.printf("%-30s%-30s%-30s%30s%n", "Virgen Blanca 63", "Cercas Bajas 68", "Nº DE PEDIDO", "1730/2019");
-        System.out.printf("%-30s%-30s%-30s%30s%n", "08759 Burgos, Burgos", "47300 Cádiz, Cádiz", "FECHA VCTO", "29-01-2019");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "08759 Burgos, Burgos", "47300 Cádiz, Cádiz", "FECHA", "29-01-2019");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "", "", "VENCIMIENTO", "");
         System.out.println();
         System.out.println();
         System.out.println("───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────");
-        System.out.printf("%-30s%-30s%-30s%30s%n", "Total Factura","","", "199,65$");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "Total Factura","","", "199,65Eur");
         System.out.println("───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────");
         System.out.println();
         System.out.println();
-        System.out.printf("%-30s%-30s%-30s%30s%n", "Cant.", "Descripcion", "Precio Unitario", "Importe");
-        System.out.printf("%-30s%-30s%-30s%30s%n", "","","100.00", "100.00");
-        System.out.printf("%-30s%-30s%-30s%30s%n", "1", "Traje de luces en rojo", "100.00", "100.00");
-        System.out.printf("%-30s%-30s%-30s%30s%n", "2", "Mui grande churrolito", "25.00", "50.00");  
-        System.out.printf("%-30s%-30s%-30s%30s%n", "3", "Equipaje de futbol", "5.00", "15.00");   
+        System.out.printf("%-30s%-30s%30s%30s%n", "Cant.", "Descripcion", "Precio", "Importe");
+        System.out.printf("%-30s%-30s%30s%30s%n", "", "", "Unitario", "");
+        System.out.printf("%-30s%-30s%30s%30s%n", "1", "Traje de luces en rojo", "100.00", "100.00");
+        System.out.printf("%-30s%-30s%30s%30s%n", "2", "Mui grande churrolito", "25.00", "50.00");  
+        System.out.printf("%-30s%-30s%30s%30s%n", "3", "Equipaje de futbol", "5.00", "15.00");   
         System.out.println();
         System.out.println();
-        System.out.printf("%-30s%-30s%-30s%30s%n", "", "", "Subtotal", "165.00");
-        System.out.printf("%-30s%-30s%-30s%30s%n", "", "", "IVA 21.0%", "34.65");
+        System.out.printf("%-30s%-30s%30s%30s%n", "", "", "Subtotal", "165.00");
+        System.out.printf("%-30s%-30s%30s%30s%n", "", "", "IVA 21.0%", "34.65");
 
     }
 }
