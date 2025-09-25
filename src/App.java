ba3f1def3109e4e1d32cc299ac200ee2d29a8474
Juan Manuel Baena <jbaeurb3009@g.educaand.es>
2025-09-25 11:17:55 +0200

    Ejercicio base factura

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Rojo Plo Paella INC.");
+        System.out.println("Carretera Muella 38");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.printf("%-30s%-30s%-30s%30s%n", "Facturar A", "Enviar A", "Nº DE FACTURA", "ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "Facturar A", "Enviar A", "Nº DE FACTURA", "ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "Facturar A", "Enviar A", "Nº DE FACTURA", "ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "Facturar A", "Enviar A", "Nº DE FACTURA", "ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "","","FECHA VCTO", "29-01-2019");
+    }
+}
