f71035b8e09547ccb3e7f87d7f7765e3abad4dc8
Juan Manuel Baena <jbaeurb3009@g.educaand.es>
2025-09-25 12:24:06 +0200

    Factura Finalizada

--- src/App.java
+++ src/App.java
@@ -6,9 +6,26 @@ public class App {
         System.out.println();
         System.out.println();
         System.out.printf("%-30s%-30s%-30s%30s%n", "Facturar A", "Enviar A", "Nº DE FACTURA", "ES-001");
-        System.out.printf("%-30s%-30s%-30s%30s%n", "Facturar A", "Enviar A", "Nº DE FACTURA", "ES-001");
-        System.out.printf("%-30s%-30s%-30s%30s%n", "Facturar A", "Enviar A", "Nº DE FACTURA", "ES-001");
-        System.out.printf("%-30s%-30s%-30s%30s%n", "Facturar A", "Enviar A", "Nº DE FACTURA", "ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "Leda Villareal", "Leda Villareal", "FECHA", "29.01.2019");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "Virgen Blanca 63", "Cercas Bajas 68", "Nº DE PEDIDO", "1730/2019");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "08759 Burgos, Burgos", "47300 Cádiz, Cádiz", "", "");
         System.out.printf("%-30s%-30s%-30s%30s%n", "","","FECHA VCTO", "29-01-2019");
+        System.out.println();
+        System.out.println();
+        System.out.println("───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "Total Factura","","", "199,65$");
+        System.out.println("───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────");
+        System.out.println();
+        System.out.println();
+        System.out.printf("%-30s%-30s%-30s%30s%n", "Cant.", "Descripcion", "Precio Unitario", "Importe");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "","","100.00", "100.00");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "1", "Traje de luces en rojo", "100.00", "100.00");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "2", "Mui grande churrolito", "25.00", "50.00");  
+        System.out.printf("%-30s%-30s%-30s%30s%n", "3", "Equipaje de futbol", "5.00", "15.00");   
+        System.out.println();
+        System.out.println();
+        System.out.printf("%-30s%-30s%-30s%30s%n", "", "", "Subtotal", "165.00");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "", "", "IVA 21.0%", "34.65");
+
     }
 }
